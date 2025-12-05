a3099549e3d174ce938e294c4aaab5853b8dabe7
Jeesyuen <25066410@siswa.um.edu.my>
2025-12-05 15:19:35 +0800

    Initial commit

---
+++ src/main/java/Tutorial6Q1.java
@@ -0,0 +1,18 @@
+public class Tutorial6Q1
+{
+    public static int maxNum(int a, int b, int c)
+    {
+        if(a>b && a>c)
+        {
+            return a;
+        }
+        else if(b>a && b>c)
+        {
+            return b;
+        }
+        else
+        {
+            return c;
+        }
+    }
+}
